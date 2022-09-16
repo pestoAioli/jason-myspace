90bf06db0e7a998334bde6297bf3d42fc4c1f4ae
Ricky <drickyriverab@gmail.com>
2022-09-16 15:27:05 -0700

    attempt to improve SEO

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ export default function Home() {
       <Head>
         <title>Jason Holtz' Homepage</title>
         <meta name='author' content='Jason Holtz'></meta>
+        <meta name='title' content='Jason Holtz Homepage'></meta>
         <meta name='description' content='Artist Jason Holtz Homepage, find contact information and browse his works of art'></meta>
         <meta property='og:type' content='website'></meta>
         <meta property='og:title' content='Jason Holtz, Visual Artist'></meta>
+        <meta property='og:site_name' content='Jason Holtz'></meta>
       </Head>
       <div className={styles.container}>
         <NavBar />
