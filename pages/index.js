7eec929196b1d43a3dc822211330361a4fa8adac
Ricky <drickyriverab@gmail.com>
2022-09-16 11:32:53 -0700

    SEO improvements

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import Footer from '../components/Footer.js'
 export default function Home() {
   return (
     <>
+      <head>
+        <meta name='keywords' content='Jason, Holtz, Artist, Petaluma, Visual Art, Nature, Graffiti, Art, Painting, Painter, Drawing, Linework'></meta>
+        <meta name='description' content='Artist Jason Holtz Homepage, find contact information and browse his works of art'></meta>
+        <title>Jason Holtz, visual artist working to explore the complexities and simplicities of life, through a variety of mediums and styles.</title>
+      </head>
       <div className={styles.container}>
         <NavBar />
         <MySpaceHome />
