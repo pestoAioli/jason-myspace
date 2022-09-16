1b02b44d2a6328e440ec4f669c23be1bc5a283dd
Ricky <drickyriverab@gmail.com>
2022-09-16 11:36:48 -0700

    SEO improvements v2

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,16 @@ import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar.js';
 import MySpaceHome from '../components/MySpaceHome.js'
 import Footer from '../components/Footer.js'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <head>
+      <Head>
+        <title>Jason Holtz, visual artist working to explore the complexities and simplicities of life, through a variety of mediums and styles.</title>
         <meta name='keywords' content='Jason, Holtz, Artist, Petaluma, Visual Art, Nature, Graffiti, Art, Painting, Painter, Drawing, Linework'></meta>
         <meta name='description' content='Artist Jason Holtz Homepage, find contact information and browse his works of art'></meta>
-        <title>Jason Holtz, visual artist working to explore the complexities and simplicities of life, through a variety of mediums and styles.</title>
-      </head>
+      </Head>
       <div className={styles.container}>
         <NavBar />
         <MySpaceHome />
